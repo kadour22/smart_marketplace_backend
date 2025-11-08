72de7b63931d28eea72b4d84dc039d79f39fc0eb
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-11-08 21:15:39 +0100

    create products serializer

---
+++ products/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Product
+
+class product_serializer(serializers.ModelSerializer) :
+    class Meta :
+        model = Product
+        fields = "__all__"
+        read_only_fields = ['embedding', 'seller']
