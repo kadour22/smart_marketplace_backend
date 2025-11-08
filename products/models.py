32dd2084a6fee51c2adbb2f176b3e25fc579596d
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-11-08 21:15:06 +0100

    create products models with embedding field

--- products/models.py
+++ products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from pgvector.django import VectorField
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Product(models.Model) :
+    product_name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6,decimal_places=2)
+    description = models.CharField(max_length=500)
+    image = models.ImageField(upload_to="products_images/")
+    embedding = VectorField() 
+
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.product_name
--- smart_marketplace/settings.py
+++ smart_marketplace/settings.py
@@ -30,7 +30,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'products',
-    'rest_framework'
+    'rest_framework',
+    'pgvector'
 ]
 
 MIDDLEWARE = [
@@ -65,14 +66,12 @@ WSGI_APPLICATION = 'smart_marketplace.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
@@ -109,7 +108,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+MEDIA_URL = "media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
