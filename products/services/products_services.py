fd773ab3848bc91e658b18d440527676da8b1e7e
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-11-23 14:04:42 +0100

    add retrieve product service logic

--- products/services/products_services.py
+++ products/services/products_services.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from products.models import Product
 from products.serializers import product_serializer
@@ -8,4 +9,9 @@ from rest_framework import status
 def list_products_service():
     products = Product.objects.all()
     serializer = product_serializer(products, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+# def product detail service
+def product_detail_service(product_id):
+    product = get_object_or_404(Product, id=product_id)
+    serializer = product_serializer(product)
     return Response(serializer.data, status=status.HTTP_200_OK)
