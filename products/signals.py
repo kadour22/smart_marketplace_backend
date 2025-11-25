27d84b1b6bdc54ad3d832e455f11c1457c7a0de1
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-11-25 21:07:20 +0100

    add singal with ai that generate descriptions

--- products/signals.py
+++ products/signals.py
@@ -11,3 +11,17 @@ def embedding_product_field(sender , instance , **kwargs) :
     instance.embedding = model.encode(text).tolist()
     print(f'{instance.embedding[:5]}')
 
+
+@receiver(pre_save, sender=Product)        
+def generate_discription_with_ai(sender, instance, **kwargs):
+        if not instance.description:
+            response = client.chat.completions.create(
+            model="x-ai/grok-4.1-fast:free",
+            messages=[
+                    {"role": "system", "content": "You are a helpful assistant that generates product descriptions."},
+                    {"role": "user", "content": f"Generate a detailed product small attractive description for a product named '{instance.product_name}'."}
+                ])
+                    
+            instance.description = response.choices[0].message.content[:-1]
+            print("Description generated and saved.")
+        print("Description already exists.") 
