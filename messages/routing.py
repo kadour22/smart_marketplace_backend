046ec3c8088892e86670620bbcf8741fa4332a9d
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-11-27 22:03:37 +0100

    setup a Notification consumer routing

--- messages/routing.py
+++ messages/routing.py
@@ -1,3 +1,6 @@
 from django.urls import re_path
+from .consumers import NotificationConsumer
 
-websocket_urlpatterns = []
+websocket_urlpatterns = [
+    re_path(r"ws/notifications/", NotificationConsumer.as_asgi())
+]
