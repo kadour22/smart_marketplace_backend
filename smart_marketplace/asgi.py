62f4d91b4a2dc7615ca4e9dd1f85a6712ac3e4f0
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-11-27 22:06:01 +0100

    asgi entry point for websocekt

--- smart_marketplace/asgi.py
+++ smart_marketplace/asgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smart_marketplace.settings')
 
-application = get_asgi_application()
+import messages.routing
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": URLRouter(
+        messages.routing.websocket_urlpatterns
+    ),
+})
