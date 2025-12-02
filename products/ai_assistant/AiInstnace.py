e1e4cdce23dbba97597f3d14325b4c4f2a70e9a9
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-12-02 10:26:36 +0100

    fixe AI instance issue

--- products/ai_assistant/AiInstnace.py
+++ products/ai_assistant/AiInstnace.py
@@ -1,6 +1,6 @@
 import os
 from openai import OpenAI
-api_key = os.getenv("OPENROUTER_API_KEY")
+api_key = os.getenv("OPENAI_API_KEY")
 
 client = OpenAI(
   base_url="https://openrouter.ai/api/v1",
