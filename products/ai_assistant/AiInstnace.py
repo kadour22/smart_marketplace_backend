1c347db61a173b8872ef5dd0f082f6d5a8db4965
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-12-06 13:48:34 +0100

    add ai api key to .env file

--- products/ai_assistant/AiInstnace.py
+++ products/ai_assistant/AiInstnace.py
@@ -1,7 +1,7 @@
 import os
 from openai import OpenAI
 
-api_key = "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"
+api_key = os.getenv("OPENAI_API_KEY")
 
 client = OpenAI(
   base_url="https://openrouter.ai/api/v1",
