470faab0f3ba068cb28f75b8e6cc62b2c8bd3936
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-12-10 13:55:25 +0100

    create RateSeller model

--- Rate/models.py
+++ Rate/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class RateSeller(models.Model) :
+    seller  = models.ForeignKey(User, on_delete=models.CASCADE , related_name='rate')
+    rate_by = models.ForeignKey(User , on_delete=models.CASCADE, related_name='my_rates') 
+    rate_value = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.seller.username}"
