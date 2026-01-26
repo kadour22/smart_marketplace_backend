8212f0b231213f6d6171471f07376f41c2bfb348
Abdelkader <zaroui.abedlkader777@gmail.com>
2026-01-26 23:38:48 +0100

    update

--- customers/views.py
+++ customers/views.py
@@ -8,8 +8,7 @@ from .services.services import (
 class CustomerProfileView(APIView):
     def get(self, request):
         return customer_profile(user=request.user)
-       
-            
+
 class customer_wishlist_view(APIView):
     def get(self, request):
         return customer_wishlist(user=request.user)
