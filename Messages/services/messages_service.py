e8e4406c47d5e39b9c302feda37b3008d075bd30
Abdelkader <zaroui.abedlkader777@gmail.com>
2026-01-26 23:39:20 +0100

    update

--- Messages/services/messages_service.py
+++ Messages/services/messages_service.py
@@ -63,5 +63,5 @@ def sending_message(data,sender) :
         with transaction.atomic():
             message = serializer.save(sender=sender)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-        print("Serializer errors:", serializer.errors)  
+          
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
